c9a15941822e0af8d59475682f1aea9ad3cfd8fb
lyra-lee <lyra.lee@kakaocorp.com>
2022-09-13 16:52:51 +0900

    요청 PATH와 일치하는 파일을 읽어 클라이언트에 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,18 +17,30 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = getBody(in);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private byte[] getBody(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        String path = getPath(br.readLine());
+        try {
+            return Files.readAllBytes(new File("./webapp" + path).toPath());
+        } catch (IOException e) {
+            return "Hello World".getBytes();
+        }
+    }
+
+    private String getPath(String line) {
+        return line.split(" ")[1];
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
