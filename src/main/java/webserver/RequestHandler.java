b3fc462003ca0b52c969c7ef305f7139aa726197
kelley.zero(김은영)/kakao <kelley.zero@kakaocorp.com>
2022-09-14 18:01:31 +0900

    step2: GET으로 회원가입 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,14 +1,21 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.file.Files;
-
+import com.google.common.base.Strings;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static final String QUERY_STRING_DELIMITER = "\\?";
 
     private final Socket connection;
 
@@ -22,14 +29,24 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             String line = br.readLine();
-            if(line == null) {
+            if (line == null) {
                 return;
             }
-            String url = line.split(" ")[1];
 
-            while (!line.equals("")) {
+            String url = HttpRequestUtils.getUrl(line);
+
+            if (url.contains("/user/create")) {
+                String queryString = url.split(QUERY_STRING_DELIMITER)[1];
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                logger.debug("User Info: {}", params);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                logger.debug("User : {}", user);
+                url = "/index.html";
+            }
+
+            while (!Strings.isNullOrEmpty(line)) {
                 line = br.readLine();
                 logger.debug("header : {}", line);
             }
