0d4124534dd7fa214c43e5b83f20237d93d844b9
kelley.zero(김은영)/kakao <kelley.zero@kakaocorp.com>
2022-09-24 15:24:04 +0900

    feat: RequestHandler 리팩토링(writeBody, writeHeader 분리 등)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,7 +1,10 @@
 package webserver;
 
-import model.User;
-import model.http.HttpRequestHeader;
+import controller.UserController;
+import model.http.request.HttpRequest;
+import model.http.request.Method;
+import model.http.response.HttpResponse;
+import model.http.response.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.HttpRequestHeaderParser;
@@ -10,7 +13,7 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Map;
+
 public class RequestHandler implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -28,44 +31,57 @@ public class RequestHandler implements Runnable {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             DataOutputStream dos = new DataOutputStream(out);
 
-            HttpRequestHeader httpRequestHeader = HttpRequestHeaderParser.parseHttpRequestHeader(br.readLine());
-            log.debug("HttpRequestHeader : {}", httpRequestHeader);
+            HttpRequest httpRequest = HttpRequestHeaderParser.parseHttpRequestHeader(br);
+            log.debug("HttpRequest : {}", httpRequest);
+            HttpResponse httpResponse = getResponse(httpRequest);
+            log.debug("HttpResponse : {}", httpResponse);
 
-            String url = httpRequestHeader.getUri();
-
-            if (url.contains("/user/create")) {
-                sendSignUpRequest(httpRequestHeader);
-            }
-
-            response200Header(dos, getRequestBody(url).length, httpRequestHeader.getContentType());
-            responseBody(dos, getRequestBody(url));
+            write(dos, httpResponse, httpRequest);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private byte[] getRequestBody(String url) throws IOException {
-        return Files.readAllBytes(new File("./webapp" + url).toPath());
+    private HttpResponse getResponse(HttpRequest httpRequest) throws IOException {
+        if (httpRequest.getUri().contains("user") && httpRequest.getMethod().equals(Method.POST))
+            return UserController.getInstance().response(httpRequest);
+
+        return new HttpResponse.HttpResponseBuilder()
+                .status(Status.OK)
+                .body(Files.readAllBytes(new File("./webapp" + File.separator + httpRequest.getUri()).toPath()))
+                .build();
     }
 
-    private void sendSignUpRequest(HttpRequestHeader httpRequestHeader) {
-        Map<String, String> params = httpRequestHeader.getParams();
-        User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
-        log.debug("User : {}", user);
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
+    private void write(DataOutputStream dos, HttpResponse httpResponse, HttpRequest httpRequest) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/" + contentType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            writeHeaders(dos, httpRequest, httpResponse);
+            writeBody(dos, httpResponse.getBody());
+            dos.flush();
         } catch (IOException e) {
-            log.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void writeHeaders(DataOutputStream dos, HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        dos.writeBytes(httpResponse.getVersion() + " " + httpResponse.getStatus().getCode() + " " + httpResponse.getStatus().getMessage() + " " + "\r\n");
+        dos.writeBytes("Content-Type: text/" + httpRequest.getContentType() + ";charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + httpResponse.getBody().length + "\r\n");
+
+        httpResponse.getHeaders().forEach(
+                (key, values) -> {
+                    String joinString = String.join(";", values);
+                    try {
+                        dos.writeBytes(key + ": " + joinString + "\r\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        dos.writeBytes("\r\n");
+    }
+
+    private void writeBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
