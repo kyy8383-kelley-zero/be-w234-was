bb59733e039d25dea23be436be4dfd73d5a1c2bd
kelley.zero(김은영)/kakao <kelley.zero@kakaocorp.com>
2022-09-24 15:25:37 +0900

    feat: UserService login, signup 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,15 +1,14 @@
 package service;
 
 import db.Database;
-import model.User;
 import error.DuplicatedUserException;
 import error.FailedLoginException;
+import model.User;
 
 public class UserService {
-    private static UserService instance = new UserService();
+    private static final UserService instance = new UserService();
 
     private UserService() {
-
     }
 
     public static UserService getInstance() {
@@ -17,17 +16,17 @@ public class UserService {
     }
 
     public User signUp(String userId, String password, String name, String email) {
+        User user = User.of(userId, password, name, email);
         if (Database.findUserById(userId) != null) {
             throw new DuplicatedUserException("이미 존재하는 user 입니다.");
         }
-        User user = User.of(userId, password, name, email);
         Database.addUser(user);
         return user;
     }
 
-    public boolean login(String userId, String password){
+    public boolean login(String userId, String password) {
         User user = Database.findUserById(userId);
-        if(user == null || !user.getPassword().equals(password)){
+        if (user == null || !password.equals(user.getPassword())) {
             throw new FailedLoginException("id나 password 를 잘못 입력하셨습니다");
         }
         return true;
