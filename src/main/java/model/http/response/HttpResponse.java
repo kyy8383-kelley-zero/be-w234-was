ebab1bb03e43494be9b84b472670c9825fafaf59
kelley.zero(김은영)/kakao <kelley.zero@kakaocorp.com>
2022-09-24 15:22:49 +0900

    feat: HttpResponseBody 추가

--- src/main/java/model/http/response/HttpResponse.java
+++ src/main/java/model/http/response/HttpResponse.java
@@ -1,25 +1,60 @@
 package model.http.response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
-    private final StatusCode statusCode;
+    private final Status status;
+    private final String version = "HTTP/1.1";
     private final Map<String, String> headers;
     private final byte[] body;
 
-    public HttpResponse( StatusCode statusCode, Map<String, String> headers, byte[] body) {
-        this.statusCode = statusCode;
-        this.headers = headers;
-        this.body = body;
+    public HttpResponse(HttpResponseBuilder builder) {
+        this.status = builder.status;
+        this.headers = builder.headers;
+        this.body = builder.body;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpResponse{" +
+                "status=" + status +
+                ", version='" + version + '\'' +
+                ", headers=" + headers +
+                '}';
+    }
+
+    public static class HttpResponseBuilder {
+        private Status status;
+        private final Map<String, String> headers = new HashMap<>();
+        private byte[] body = new byte[0];
+
+        public HttpResponseBuilder status(Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public HttpResponseBuilder headers(String key, String value) {
+            this.headers.put(key, value);
+            return this;
+        }
+
+        public HttpResponseBuilder body(byte[] body) {
+            this.body = body;
+            return this;
+        }
+
+        public HttpResponse build() {
+            return new HttpResponse(this);
+        }
     }
 
     public String getVersion() {
-        String version = "HTTP/1.1";
         return version;
     }
 
-    public StatusCode getStatusCode() {
-        return statusCode;
+    public Status getStatus() {
+        return status;
     }
 
     public Map<String, String> getHeaders() {
@@ -29,6 +64,7 @@ public class HttpResponse {
     public byte[] getBody() {
         return body;
     }
+
 
 }
 
